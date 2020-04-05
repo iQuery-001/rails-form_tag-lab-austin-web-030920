fa560a88c93950ad7727937825e4b06d9898151f
iquery-001 <kota@vaga.tk>
2020-04-04 19:14:16 -0500

    Done.

--- .history/app/controllers/students_controller_20200404184051.rb
+++ .history/app/controllers/students_controller_20200404184051.rb
@@ -11,9 +11,4 @@ class StudentsController < ApplicationController
   def new
   end
 
-  def create
-    @student = Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
-    redirect_to student_path(@student)
-  end
-
 end
